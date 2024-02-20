7927fd30387a42f9638a319be481d94dd3ad343f
Eng. Elias <eng.elias.owis@gmail.com>
2024-02-20 20:33:08 +0400

    Singlton Prisma Client

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma: PrismaClient = new PrismaClient();
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
